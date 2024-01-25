8a0a1adb5ebef7e84cfa7c6618b530a64eb6d2ac
filipegst <filipegst@live.com>
2024-01-25 11:43:14 -0300

    Desafio: implementando design com técnicas similares

--- app/src/main/java/br/com/example/aluvery_jetpackcompose/MainActivity.kt
+++ app/src/main/java/br/com/example/aluvery_jetpackcompose/MainActivity.kt
@@ -3,17 +3,41 @@ package br.com.example.aluvery_jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.example.aluvery_jetpackcompose.ui.theme.Aluvery_JetpackComposeTheme
+import br.com.example.aluvery_jetpackcompose.ui.theme.Roxo
+import br.com.example.aluvery_jetpackcompose.ui.theme.Teal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,81 +49,91 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    TextoColuna()
-                    TextoLinha()
-                    TextoCaixa()
+
                 }
             }
         }
     }
 }
 
-    @Composable
-    fun TextoColuna() {
-        Column {
-            Text(text = "Hello")
-            Text(text = "Moda")
-            Text(text = "Foca")
+@Preview(showBackground = true)
+@Composable
+fun ProductItem() {
+
+    Surface(
+        Modifier.padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        shadowElevation = 4.dp
+    ) {
+
+        Column(
+            Modifier
+                .heightIn(250.dp, 310.dp)
+                .width(200.dp)
+        )
+
+        {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.horizontalGradient(colors = listOf(Roxo, Teal))
+                    )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize/2)
+                        .clip(shape = CircleShape)
+                        .align(BottomCenter)
+                )
 
             }
-        }
-
-    @Composable
-    fun TextoLinha() {
-        Row{
-            Text(text = "Hello")
-            Text(text = "Moda")
-            Text(text = "Foca")
-
-        }
-    }
-
-    @Composable
-    fun TextoCaixa() {
-        Box{
-            Text(text = "Hello")
-            Text(text = "Moda")
-            Text(text = "Foca")
+            Spacer(modifier = Modifier.height(imageSize/2))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(10).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 2
+                )
+                Text(
+                    text = "TEXTO 2",
+                    modifier = Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
 
         }
     }
 
+}
 
 @Preview(showBackground = true)
 @Composable
-fun CustomLayoutPreview() {
-    Column {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-        Row {
-            Text(text = "Texto 3")
-            Text(text = "Texto 4")
-        }
-        Box {
-            Row {
-                Text(text = "Texto 5")
-                Text(text = "Texto 6")
-            }
-            Column {
-                Text(text = "Texto 7")
-                Text(text = "Texto 8")
-            }
+fun DesafioUm() {
+    Row {
+        Box(
+            modifier = Modifier
+                .height(160.dp)
+                .background(Color.Blue)
+                .width(100.dp)
+        )
+        Column() {
+            Text(
+                text = "TEXTO 1",
+                Modifier
+                    .background(Color.Gray)
+                    .padding(16.dp)
+            )
+            Text(text = "TEXTO 2", Modifier.padding(16.dp))
+
         }
     }
 }
-
-    @Preview(name = "TextoPreview",showBackground = true)
-    @Composable
-    fun TextoPreview() {
-            TextoColuna()
-    }
-    @Preview(name = "TextoLinhaPreview",showBackground = true)
-    @Composable
-    fun TextoLinhaPreview() {
-            TextoLinha()
-    }
-    @Preview(name = "TextoCaixaPreview",showBackground = true)
-    @Composable
-    fun TextoCaixaPreview() {
-            TextoCaixa()
-    }
--- app/src/main/java/br/com/example/aluvery_jetpackcompose/ui/theme/ui/theme/Color.kt
+++ app/src/main/java/br/com/example/aluvery_jetpackcompose/ui/theme/ui/theme/Color.kt
@@ -1,13 +1,10 @@
-package br.com.example.aluvery_jetpackcompose.ui.theme
+package br.com.example.aluvery_jetpackcompose.ui.theme.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-val Roxo = Color(0xFF620EEE)
-val Teal = Color(0xFF03DAC5)
-
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
